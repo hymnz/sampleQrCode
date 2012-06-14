501155194d139913d68659f3fae67188f860e852
Hymn <Hymn@pongsils-MacBook-Pro.local>
2012-06-14 20:50:28 +0700

    Initial Commit

---
+++ firstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstApp
+//
+//  Created by pongsil vachirajongkol on 6/14/55 BE.
+//  Copyright (c) 2555 pongsil.v@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
